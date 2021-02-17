2e982155236365e5e0cc51396a57d080af08d00b
Vincent Klimjack <vklimjack@gmail.com>
2021-02-17 19:34:59 +0000

    add Queen initialize method

---
+++ lib/queen.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Queen piece object
+class Queen
+  attr_reader :marker, :color
+
+  def initialize(marker, color)
+    # use "\u2655" for black, "\u265B" for white
+    @marker = marker
+    @color = color
+  end
+end
