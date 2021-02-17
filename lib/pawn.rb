bea212c8396952581dac04ca3234c328b82a719c
Vincent Klimjack <vklimjack@gmail.com>
2021-02-17 19:37:02 +0000

    add Pawn initialize method

---
+++ lib/pawn.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Pawn piece object
+class Pawn
+  attr_reader :marker, :color
+
+  def initialize(marker, color)
+    # use "\u2659" for black, "\u265F" for white
+    @marker = marker
+    @color = color
+  end
+end
