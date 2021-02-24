abdeafdb73f5312e43f484ac3114a93099ca1520
Vincent Klimjack <vklimjack@gmail.com>
2021-02-24 10:43:50 +0000

    add tests for #valid_knight_move?

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -12,5 +12,19 @@ describe Knight do
         expect(result).to be true
       end
     end
+
+    context 'when the move is backwards 2, over 1' do
+      it 'is a valid move' do
+        result = knight.valid_knight_move?([2, 7], [0, 6])
+        expect(result).to be true
+      end
+    end
+
+    context 'when the move is invalid' do
+      it 'in not a valid move' do
+        result = knight.valid_knight_move?([0, 6], [3, 7])
+        expect(result).to be false
+      end
+    end
   end
 end
