32cb890f4962fc4223e4ce12a45e01976cf9179d
Vincent Klimjack <vklimjack@gmail.com>
2021-02-26 10:27:26 +0000

    add remaining tests for #valid_coordinates?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -29,6 +29,48 @@ describe Board do
         expect(result).to be true
       end
     end
+
+    context 'when the start and end coordinates are the same' do
+      it 'is not valid coordinates' do
+        result = board.valid_coordinates?([7, 1, 7, 1], player)
+        expect(result).to be false
+      end
+    end
+
+    context 'when the start and end coordinates are different' do
+      it 'is valid coordinates' do
+        result = board.valid_coordinates?([7, 1, 5, 0], player)
+        expect(result).to be true
+      end
+    end
+
+    context 'when the piece selected is not the current player color' do
+      it 'is not valid coordinates' do
+        result = board.valid_coordinates?([0, 1, 2, 0], player)
+        expect(result).to be false
+      end
+    end
+
+    context 'when the piece selected is the current player color' do
+      it 'is valid coordinates' do
+        result = board.valid_coordinates?([7, 1, 5, 2], player)
+        expect(result).to be true
+      end
+    end
+
+    context 'when the starting cell is an empty cell' do
+      it 'is not valid coordinates' do
+        result = board.valid_coordinates?([5, 0, 4, 0], player)
+        expect(result).to be false
+      end
+    end
+
+    context 'when the target cell is occupied' do
+      it 'is not valid coordinates' do
+        result = board.valid_coordinates?([7, 1, 6, 3], player)
+        expect(result). to be false
+      end
+    end
   end
 end
 
