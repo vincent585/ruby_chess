8ffba2c0de919a28d357938129ae0ca64f71fde5
Vincent Klimjack <vklimjack@gmail.com>
2021-03-01 17:43:21 +0000

    add king_spec and King #valid_move?

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -4,20 +4,26 @@ require_relative 'piece'
 
 # King piece object
 class King < Piece
+  MOVES =
+    [
+      [1, 1], [-1, 1], [1, -1], [-1, -1], # diagonal moves
+      [1, 0], [-1, 0], [0, 1], [0, -1] # horizontal and vertical moves
+    ].freeze
+
   attr_reader :marker, :color, :possible_moves
 
   def initialize(marker, color)
     # use "\u2654" for black and "\u265A" for white
     super(marker, color)
-    @possible_moves = []
   end
 
   def valid_move?(current_position, target)
-    @possible_moves = find_possible_moves(current_position)
-    possible_moves.include?(target)
+    MOVES.include?(coordinate_difference(current_position, target))
   end
 
-  def find_possible_moves(current_position)
-    # TODO
+  private
+
+  def coordinate_difference(current_position, target)
+    current_position.zip(target).map { |x, y| y - x }
   end
 end
