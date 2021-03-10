4664135b763dfcb37d76298a6f202b54cafd911b
Vincent Klimjack <vklimjack@gmail.com>
2021-03-10 21:16:42 +0000

    add MOVE constant for Bishop, to add move generating method

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -4,10 +4,21 @@ require_relative 'piece'
 
 # Bishop piece object
 class Bishop < Piece
-  attr_reader :marker, :color
+  MOVES = [[1, 1], [1, -1], [-1, 1], [-1, -1]].freeze
+
+  attr_reader :marker, :color, :possible_moves
 
   def initialize(marker, color)
     # use "\u2657" for black, "\u265D" for white
     super(marker, color)
+    @possible_moves = []
+  end
+
+  def valid_move?(current_position, target)
+    possible_moves.each { |diagonal| diagonal.include?(target) }
+  end
+
+  def get_moves(current_position)
+    # TODO
   end
 end
