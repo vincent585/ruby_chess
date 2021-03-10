308ab6683501caea637b43101713c06e339a8b61
Vincent Klimjack <vklimjack@gmail.com>
2021-03-10 18:32:57 +0000

    add other true #valid_move? tests

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
+
 require './lib/pieces/king'
 
 describe King do
@@ -12,5 +14,28 @@ describe King do
         expect(result).to be true
       end
     end
+
+    context 'when the move is backward one space' do
+      it 'is a valid move' do
+        result = king.valid_move?([6, 4], [7, 4])
+        expect(result).to be true
+      end
+    end
+
+    context 'when the move is horizontal one space' do
+      it 'is a valid move' do
+        result = king.valid_move?([7, 4], [7, 3])
+        expect(result).to be true
+      end
+    end
+
+    context 'when the move is diagonal one space' do
+      it 'is a valid move' do
+        result = king.valid_move?([7, 4], [6, 5])
+        expect(result).to be true
+      end
+    end
   end
 end
+
+# rubocop:enable Metrics/BlockLength
