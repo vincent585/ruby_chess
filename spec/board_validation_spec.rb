20a48266ed367b89544619e2a79f39a631e00729
Vincent Klimjack <vklimjack@gmail.com>
2021-04-05 17:34:14 +0100

    add remainder of tests for #valid_pawn_move?

--- spec/board_validation_spec.rb
+++ spec/board_validation_spec.rb
@@ -9,13 +9,11 @@ describe BoardValidation do
   let(:dummy_player) { Player.new(1, 'black') }
 
   describe '#valid_pawn_move?' do
-    before do
-      dummy_board.cells[1][0] = Pawn.new("\u2659", 'black')
-    end
+    before { dummy_board.cells[1][0] = Pawn.new("\u2659", 'black') }
+    let(:pawn) { dummy_board.cells[1][0] }
 
     context 'when the move is forward one space, unblocked' do
       it 'is a valid move' do
-        pawn = dummy_board.cells[1][0]
         result = dummy_board.valid_pawn_move?([1, 0], [2, 0], pawn, dummy_player)
         expect(result).to be true
       end
@@ -23,7 +21,6 @@ describe BoardValidation do
 
     context 'when the move is forward two spaces, unblocked' do
       it 'is a valid move' do
-        pawn = dummy_board.cells[1][0]
         result = dummy_board.valid_pawn_move?([1, 0], [3, 0], pawn, dummy_player)
         expect(result).to be true
       end
@@ -32,9 +29,38 @@ describe BoardValidation do
     context 'when the move is diagonal one space, and there is an enemy piece there' do
       before { dummy_board.cells[2][1] = Pawn.new("\u2659", 'white') }
       it 'is a valid move' do
-        pawn = dummy_board.cells[1][0]
         result = dummy_board.valid_pawn_move?([1, 0], [2, 1], pawn, dummy_player)
         expect(result).to be true
+      end
+    end
+
+    context 'when the move is diagonal one space, but no enemy piece is there' do
+      it 'is not a valid move' do
+        result = dummy_board.valid_pawn_move?([1, 0], [2, 1], pawn, dummy_player)
+        expect(result).to be false
+      end
+    end
+
+    context 'when the move is backward one space' do
+      it 'is not a valid move' do
+        result = dummy_board.valid_pawn_move?([1, 0], [0, 0], pawn, dummy_player)
+        expect(result).to be false
+      end
+    end
+
+    context 'when the move is forward two spaces but the path is blocked' do
+      before { dummy_board.cells[2][0] = Pawn.new("\u2659", 'black') }
+      it 'is not a valid move' do
+        result = dummy_board.valid_pawn_move?([1, 0], [3, 0], pawn, dummy_player)
+        expect(result).to be false
+      end
+    end
+
+    context 'when the move is forward one space but the space is occupied' do
+      before { dummy_board.cells[2][0] = Pawn.new("\u2659", 'white') }
+      it 'is not a valid move' do
+        result = dummy_board.valid_pawn_move?([1, 0], [2, 0], pawn, dummy_player)
+        expect(result).to be false
       end
     end
   end
