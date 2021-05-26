5636322b62f58a54300484f7bfd23f5f68eff4ed
Vincent Klimjack <vklimjack@gmail.com>
2021-05-26 13:43:01 +0100

    add tests for find_active_pieces

--- spec/check_detection_spec.rb
+++ spec/check_detection_spec.rb
@@ -2,6 +2,8 @@
 
 require './lib/game'
 
+# rubocop:disable Metrics/BlockLength
+
 describe CheckDetection do
   let(:dummy_game) { Game.new }
   let(:player_one) { Player.new(1, 'white') }
@@ -12,4 +14,30 @@ describe CheckDetection do
     dummy_game.instance_variable_set(:@players, [player_one, player_two])
     dummy_game.instance_variable_set(:@current_player, player_one)
   end
+
+  describe '#find_active_pieces' do
+    context 'when all the pieces remain' do
+      it 'adds all 32 pieces to the active pieces' do
+        dummy_game.find_active_pieces
+        expect(dummy_game.active_pieces.length).to eq(32)
+      end
+    end
+
+    context 'when a piece is removed' do
+      before do
+        dummy_game.board.cells[7][4] = '   '
+      end
+      it 'the size of active pieces decreases by one' do
+        dummy_game.find_active_pieces
+        expect(dummy_game.active_pieces.length).to eq(31)
+      end
+    end
+  end
+
+  describe '#find_attacking_pieces' do
+    # edit board cells to reflect attacking positions/non-attacking for each example
+    # TODO
+  end
 end
+
+# rubocop:enable Metrics/BlockLength
